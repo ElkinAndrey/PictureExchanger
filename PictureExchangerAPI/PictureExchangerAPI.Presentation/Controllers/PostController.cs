2b5e8424e4606c3d61c42dcc59523c5c5ac64b4b
Ridbir <andreyridbir@gmail.com>
2023-11-06 20:36:42 +0500

    Добавил конечную точку для получения списка постов

---
+++ PictureExchangerAPI/PictureExchangerAPI.Presentation/Controllers/PostController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PictureExchangerAPI.Presentation.DTO.Posts;
+
+namespace PictureExchangerAPI.Presentation.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с постами
+    /// </summary>
+    [Route("api/posts")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        /// <summary>
+        /// Получить список постов
+        /// </summary>
+        /// <param name="model">Начало отчета, конец отчета, часть названия</param>
+        /// <returns>Список с Id, названием, датой, приватностью, забанен ли, тегами, картинками, пользователем</returns>
+        [HttpPost("")]
+        public IActionResult Get(GetPostsDto model)
+        {
+            var posts = new List<object>()
+            {
+                new
+                {
+                    Id = new Guid("00000000-0000-0000-0000-000000000001"),
+                    Name = "Аниме картинки",
+                    Date = new DateTime(2023, 6, 13, 20, 30, 59),
+                    IsPrivate = false,
+                    IsBanned = false,
+                    Tags = new List<object>() { "аниме", "картинка" },
+                    Images = new List<object>() { 1, 2, 3 },
+                    User = new
+                    {
+                        Id = new Guid("00000000-0000-0000-0000-000000000002"),
+                        Name = "Igor123",
+                    },
+                },
+                new
+                {
+                    Id = new Guid("00000000-0000-0000-0000-000000000002"),
+                    Name = "CS GO",
+                    Date = new DateTime(2023, 6, 13, 20, 30, 59),
+                    IsPrivate = false,
+                    IsBanned = false,
+                    Tags = new List<object>() { "cs go", "cs", "go" },
+                    Images = new List<object>() { 1, 2 },
+                    User = new
+                    {
+                        Id = new Guid("00000000-0000-0000-0000-000000000000"),
+                        Name = "Vasya000",
+                    },
+                },
+            };
+
+            return Ok(posts);
+        }
+    }
+}
---
+++ PictureExchangerAPI/PictureExchangerAPI.Presentation/DTO/Posts/GetPostsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PictureExchangerAPI.Presentation.DTO.Posts
+{
+    /// <summary>
+    /// Dto для получения постов
+    /// </summary>
+    /// <param name="start">Начало отчета</param>
+    /// <param name="length">Длина среза</param>
+    /// <param name="name">Часть названия</param>
+    public record class GetPostsDto(
+        int start,
+        int length,
+        string name);
+}
