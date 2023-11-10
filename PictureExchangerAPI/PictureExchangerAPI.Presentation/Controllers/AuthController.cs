4cba5f47b6f8c7ff10592e9d8727f402251ba0a7
Ridbir <andreyridbir@gmail.com>
2023-11-10 19:46:32 +0500

    Добавил конечную точку для регистрации

---
+++ PictureExchangerAPI/PictureExchangerAPI.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Win32;
+using PictureExchangerAPI.Presentation.DTO.Auth;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace PictureExchangerAPI.Presentation.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        /// <summary>
+        /// Зарегистрироваться
+        /// </summary>
+        /// <param name="model">Имя, электронная почта и пароль</param>
+        /// <returns>Токен</returns>
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            var secretKey = "secretKeysecretKeysecretKeysecretKeysecretKeysecretKeysecretKeysecretKeysecretKeysecretKeysecretKeysecretKey";
+            
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "123"), 
+                new Claim(ClaimTypes.Email, "123"), 
+                new Claim(ClaimTypes.Role, "123") 
+            };
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.Add(new TimeSpan(100, 0, 0, 0)),
+                signingCredentials: creds);
+            var access = new JwtSecurityTokenHandler().WriteToken(token);
+            var refresh = "123";
+
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true, 
+                Expires = DateTime.Now.Add(new TimeSpan(200, 0, 0, 0)),
+            };
+            Response.Cookies.Append("refreshToken", refresh, cookieOptions);
+            return Ok(access);
+        }
+    }
+}
---
+++ PictureExchangerAPI/PictureExchangerAPI.Presentation/DTO/Auth/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PictureExchangerAPI.Presentation.DTO.Auth
+{
+    /// <summary>
+    /// Dto для регистрации
+    /// </summary>
+    /// <param name="name">Имя пользователя</param>
+    /// <param name="email">Электронная почта</param>
+    /// <param name="password">Пароль</param>
+    public record class RegisterDto(
+        string name = "",
+        string email = "",
+        string password= "");
+}
