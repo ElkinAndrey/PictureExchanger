6c43420b7887944656909ad05463e5d0a79981de
Ridbir <andreyridbir@gmail.com>
2023-12-26 14:01:52 +0500

    Добавил интерфейс для сервиса авторизации

---
+++ PictureExchangerAPI/PictureExchangerAPI.Service/Abstractions/IAuthService.cs
@@ -0,0 +1,52 @@
+﻿using PictureExchangerAPI.Service.DTO;
+
+namespace PictureExchangerAPI.Service.Abstractions
+{
+    /// <summary>
+    /// Интерфейс для сервиса авторизации
+    /// </summary>
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Зарегистрироваться
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="email">Электронная почта</param>
+        /// <param name="password">Пароль</param
+        /// <param name="secretKey">Секретный ключ</param>
+        /// <returns>Пара токенов</returns>
+        public Task<PairOfTokens> RegisterAsync(
+            string name,
+            string email,
+            string password,
+            string secretKey);
+
+        /// <summary>
+        /// Войти в аккаунт
+        /// </summary>
+        /// <param name="nameOrEmail">Имя пользователя или электронная почта</param>
+        /// <param name="password">Пароль</param
+        /// <param name="secretKey">Секретный ключ</param>
+        /// <returns>Пара токенов</returns>
+        public Task<PairOfTokens> LoginAsync(
+            string nameOrEmail,
+            string password,
+            string secretKey);
+
+        /// <summary>
+        /// Удалить токен обновления из базы данных
+        /// </summary>
+        /// <param name="token">Токен</param>
+        public Task DeleteTokenAsync(string token);
+
+        /// <summary>
+        /// Обновить токен
+        /// </summary>
+        /// <param name="token">Токен</param>
+        /// <param name="secretKey">Секретный ключ для формирования Access токена</param>        
+        /// <returns>Пара токенов</returns>
+        public Task<PairOfTokens> RefreshTokenAsync(
+            string token,
+            string secretKey);
+    }
+}
---
+++ PictureExchangerAPI/PictureExchangerAPI.Service/DTO/PairOfTokens.cs
@@ -0,0 +1,13 @@
+﻿namespace PictureExchangerAPI.Service.DTO
+{
+    /// <summary>
+    /// Пара токенов
+    /// </summary>
+    /// <param name="AccessToken">Токен доступа</param>
+    /// <param name="RefreshToken">Токен обновления</param>
+    /// <param name="DateOfCreation">Дата создания</param>
+    public partial record class PairOfTokens(
+        string AccessToken,
+        string RefreshToken,
+        DateTime DateOfCreation);
+}
